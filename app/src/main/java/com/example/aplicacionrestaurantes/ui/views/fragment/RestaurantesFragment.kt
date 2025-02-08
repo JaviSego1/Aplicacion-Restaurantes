700f46af9ee33a8a32f88ee29e9e2889e1fed4f3
JaviSego1 <javiersegoviamartinez@gmail.com>
2025-02-08 22:46:03 +0100

    TERMINADO VESION 1.4

--- app/src/main/java/com/example/aplicacionrestaurantes/ui/views/fragment/RestaurantesFragment.kt
+++ app/src/main/java/com/example/aplicacionrestaurantes/ui/views/fragment/RestaurantesFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.aplicacionrestaurantes.R
 import com.example.aplicacionrestaurantes.data.models.Restaurante
 import com.example.aplicacionrestaurantes.databinding.FragmentRestaurantesBinding
@@ -41,6 +42,8 @@ class RestaurantesFragment : Fragment(R.layout.fragment_restaurantes) {
     private fun setupRecyclerView() {
         adapter = RestauranteAdapter(emptyList(), ::onDeleteRestaurant, ::onEditRestaurant)
         binding.recyclerView.adapter = adapter
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun setupAddButton() {
