f290b7e9b537e2bc00a28df3f98c107aed82ac68
JaviSego1 <javiersegoviamartinez@gmail.com>
2025-02-08 15:23:25 +0100

    Metiendo imagenes

--- app/src/main/java/com/example/aplicacionrestaurantes/ui/adapter/RestauranteViewHolder.kt
+++ app/src/main/java/com/example/aplicacionrestaurantes/ui/adapter/RestauranteViewHolder.kt
@@ -1,6 +1,5 @@
 package com.example.aplicacionrestaurantes.ui.adapter
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.aplicacionrestaurantes.databinding.ItemRestauranteBinding
@@ -13,9 +12,11 @@ class RestauranteViewHolder(private val binding: ItemRestauranteBinding) : Recyc
         binding.titulo.text = restaurante.titulo
         binding.descripcion.text = restaurante.descripcion
 
-        // Cargar imagen usando Glide (en caso de ser una URL o recurso local)
+        // Cargar imagen usando Glide desde los recursos locales (drawable)
+        val resId = binding.imagenRestaurante.context.resources.getIdentifier(
+            restaurante.imagen, "drawable", binding.imagenRestaurante.context.packageName)
         Glide.with(itemView.context)
-            .load(restaurante.imagen) // Aquí puede ser una URL o recurso drawable
+            .load(resId) // Usamos el identificador del recurso drawable
             .centerCrop()
             .into(binding.imagenRestaurante)
 
